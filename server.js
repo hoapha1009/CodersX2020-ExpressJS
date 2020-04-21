6a47e2578404e8e166f7c617a6419bb7c775c273
Glitch (bai3-query-parameters3) <none>
2020-04-21 13:34:54 +0000

    Bai3-QueryParameters

--- server.js
+++ server.js
@@ -3,21 +3,34 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require('express');
+const express = require("express");
 const app = express();
 
-//Template engine: Pug
-app.set('view engine', 'pug');
-app.set('views', './views');
+//Use pug template engine
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+var todosList = [
+  {name: "Đi chợ"}, 
+  {name: "Nấu cơm"}, 
+  {name: "Rửa bát"}, 
+  {name: "Học code tại CodersX"}, 
+];
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get('/', (request, response) => {
-  response.send('I love CodersX');
+app.get("/", (request, response) => {
+  response.send("I love CodersX");
 });
 
+//Render route todos with pug
+app.get("/todos", (req, res) => res.render("todos/index", { todosList: todosList }));
 
-app.get('/todos', (req, res) => {
-  res.render('todos/index');
+//Search in route todos by query para
+app.get('/todos/search', (req, res) => {
+  var q = req.query.q;
+  var matchedTodos = todosList.filter( function(todo){ return todo.name.toLowerCase().indexOf(q.toLowerCase()) !== -1; });
+  
+  res.render('todos/index', { todosList: matchedTodos, inputValue: q });
 });
 
 // listen for requests :)
