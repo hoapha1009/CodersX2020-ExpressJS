85d263851eb63e0becc17ec75ae78b219d278727
Glitch (bai2-template-engines2) <none>
2020-04-21 13:31:27 +0000

    Bai2-TemplateEngines

--- server.js
+++ server.js
@@ -3,18 +3,21 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
+const express = require('express');
 const app = express();
 
+//Template engine: Pug
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 // https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.send("I love CodersX");
+app.get('/', (request, response) => {
+  response.send('I love CodersX');
 });
 
-app.get("/todos", (req, res) => {
-  res.send(
-    "<h1>Todo List</h1><ul><li>Đi chợ</li><li>Nấu cơm</li><li>Rửa bát</li><li>Học code tại CodersX</li></ul>"
-  );
+
+app.get('/todos', (req, res) => {
+  res.render('todos/index');
 });
 
 // listen for requests :)
