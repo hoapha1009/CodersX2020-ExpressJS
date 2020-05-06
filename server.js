0c64ef74cd8072841c88c46b75e9a35148d8a5ba
HoangPham <hoapha1009@gmail.com>
2020-05-06 20:39:32 +0700

    Finish api users books

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const apiLogin = require("./api/routes/auth.route");
 const apiTransactions = require("./api/routes/transactions.route");
+const apiUsers = require("./api/routes/users.route");
+const apiBooks = require("./api/routes/books.route");
 
 app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(sessionMiddleware);
@@ -47,6 +49,8 @@ app.use('/cart', cartRoute);
 
 app.use("/api/login", apiLogin);
 app.use("/api/transactions", apiTransactions);
+app.use("/api/users", apiUsers);
+app.use("/api/books", apiBooks);
 
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
