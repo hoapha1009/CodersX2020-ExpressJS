83b7ed315ad69888e47203edaf1306eeb2ce8387
HoangPham <hoapha1009@gmail.com>
2020-05-08 09:53:38 +0700

    Finish bai 28

--- server.js
+++ server.js
@@ -48,6 +48,31 @@ app.use('/cart', cartRoute);
 app.use("/api/login", apiLogin);
 app.use("/api/transactions", apiTransactions);
 
+//test error
+app.use(logErrors)
+app.use(clientErrorHandler)
+app.use(errorHandler)
+
+app.use((req,res,next)=>{
+  res.status(404).render('errors', {errors: "404 Error"})
+})
+
+function logErrors (err, req, res, next) {
+  console.error(err.stack)
+  next(err)
+}
+function clientErrorHandler (err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ errors: 'Something failed!' })
+  } else {
+    next(err)
+  }
+}
+function errorHandler (err, req, res, next) {
+  res.status(500).render('errors', {errors: "500 Error"})
+}
+
+
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
 });
